96dff39292a7745258d817de570b8a55f834f014
Andrew Seier <andseier@gmail.com>
2019-10-02 12:49:30 -0700

    Prepare for global publication.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,30 @@
+/* eslint-env node */
 module.exports = {
   root: true,
-  extends: ['eslint-config-netflix-data'],
+  extends: 'eslint:recommended',
+  rules: {
+    eqeqeq: ['error', 'allow-null'],
+    'no-console': 'warn',
+    'no-redeclare': ['error', { builtinGlobals: true }],
+    'no-shadow': [
+      'warn',
+      { builtinGlobals: true, hoist: 'functions', allow: [] },
+    ],
+    'no-undef': 'error',
+    'no-undef-init': 'error',
+    'no-unused-vars': 'warn',
+    'no-var': 'error',
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+    'prefer-const': 'error',
+    'require-yield': 1,
+    'no-prototype-builtins': 1,
+  },
   env: {
     node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
   },
 };
